9dc68fcd1b4a26b754f9d166b03897c61b007be9
Kysadro (Luna) <70448984+kysadro@users.noreply.github.com>
2020-11-13 16:07:45 +0200

    Fixed some things

--- brainfog.js
+++ brainfog.js
@@ -1,29 +1,49 @@
+// Brainfog-JS, owned by Kysadro
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-const alphabet = "abcdefghijklmnopqrstuvwxyz"; // + "!@#$%^&*()_+-={}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const specialCharacters = "!@#$%^&*()_+={}[]<>/";
 
 module.exports = {
-    run: (times = 1000, complexity = 16, returnResult = true) => {
+    /**
+     * Generates a bunch of random strings and returns them in an array
+     * @param {Number} times - The times it should run (and the overall length of the array it returns)
+     * @param {Number} complexity - The length of each string
+     * @param {Boolean} randomCase - If it should switch between upper-case and lower-case randomly
+     * @param returnResult - If it should return anything
+     * @returns {[]}
+     */
+    run: (times = 1000, complexity = 16, randomCase = true, returnResult = true) => {
         let data = [];
         for (let i = 0; i < times; i++) {
             let chunk = "";
             for (let c = 0; c < complexity; c++) {  // C++ ayy~
-                chunk += alphabet[getRandomInt(0, alphabet.length - 1)];
+                chunk += ((randomCase)
+                    ? (alphabet.toLowerCase() + alphabet.toUpperCase())[getRandomInt(0, (alphabet.length * 2) - 1)]
+                    : alphabet[getRandomInt(0, alphabet.length - 1)]);
             }
             data.push(chunk);
         }
         if (returnResult) return data;
     },
-    runOnce: (complexity = 16, returnResult = true) => {
+    /**
+     * Generate one random string
+     * @param {Number} length - The length of the random string
+     * @param {Boolean} randomCase - If it should switch between upper-case and lower-case randomly
+     * @returns {String} Random string
+     */
+    runOnce: (length = 16, randomCase = true) => {
         let data = "";
-        for (let c = 0; c < complexity; c++) {  // C++ ayy~
-            data += alphabet[getRandomInt(0, alphabet.length - 1)];
+        for (let c = 0; c < length; c++) {
+            data += ((randomCase)
+                ? (alphabet.toLowerCase() + alphabet.toUpperCase())[getRandomInt(0, (alphabet.length * 2) - 1)]
+                : alphabet[getRandomInt(0, alphabet.length - 1)]);
         }
-        if (returnResult) return data;
+        return data;
     },
     /**
      * Generates a random license key
@@ -50,31 +70,5 @@ module.exports = {
             if (i < chunks - 1) { data += splitChar; }
         }
         return data;
-    },
-    /**
-     * Create a random array using the arguments
-     * @param {String[]} data - The template
-     * @returns {String[]}
-     */
-    runUseFormat: (data = ["hello", "world"]) => {
-        let results = [];
-
-        //console.log(data.length - 1);
-        for (let y = 0; y < data.length; y++) {
-            let result = "";
-            let toDecrypt = "";
-            for (let i = 0; i < data[y].length; i++) {
-                toDecrypt += alphabet[getRandomInt(0, alphabet.length - 1)];
-            }
-
-            while (result !== toDecrypt) {
-                result = "";
-                for (let x = 0; x < data[y].length; x++) {
-                    result += alphabet[getRandomInt(0, alphabet.length - 1)];
-                }
-            } // If it finally does:
-            results.push(result);
-        }
-        return results;
     }
 }
